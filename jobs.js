eee985ec14be3e12ee95796318f427a5bebe086b
Jon Crawford <jon@storenvy.com>
2014-07-23 17:58:32 -0700

    Added mobile designer and iOS engineer

--- jobs.js
+++ jobs.js
@@ -7,7 +7,7 @@ _StorenvyJobs({
   "Engineering": [
 
   {
-    "title": "VPE of Engineering",
+    "title": "VP of Engineering",
     "permalink" : "vpe",
     "pitch" : "Storenvy is hiring its first VP of Engineering to assume all responsibilities over our engineering team including personnel management, culture development, and workflow design. Being a small team, the ideal candidate would also have strong experience with Ruby on Rails and be excited to write code regularly as an individual contributor until the engineering team scales to a size that does not allow it. This is a pivotal moment in the development of Storenvy’s product and engineering culture, and this role will allow the ideal candidate to create big impact in a fast-growing company serving an important mission.<br/><br/>Our Engineering Culture<br/>First and foremost, this is your show. When you hop into your role at Storenvy, you will be allowed to define the engineering culture and process that works best for you to deliver results to the business. Currently, we adhere to strict-ish code review and testing practices with a weekly sprint cycle. We use automated testing and automated code quality review. And all of these details are subject to change under your purview as the VP of Engineering.",
     "responsibilities" : [
@@ -27,6 +27,32 @@ _StorenvyJobs({
     ],
     "apply" : "For this kind of role, it's so much about culture fit and clicking with the leadership and team. Just drop me (the CEO) an email at <a href=\"mailto:jon@storenvy.com\">jon@storenvy.com</a> and we'll jump on a call or grab a beer and take it from there!"
   },
+
+  {
+  /* iOS Engineer */
+       "title": "Lead iOS Engineer",
+       "permalink": "lead-ios",
+       "pitch": "The desktop web is so last year. Storenvy is headed to native mobile apps and we need your help taking us there. We have a great engineering team of 5 full-stack engineers, but we're bringing on someone to own our upcoming mobile app(s). These apps will be under constant development and will always be improving. Help us make commerce accessible to everyone and introduce amazing businesses to the world.",
+
+       "responsibilities": [
+         "Own the development and maintenance of the upcoming Storenvy mobile apps.",
+         "Write test-driven, clean, maintainable code.",
+         "Constantly look for ways to improve user experience.",
+         "Work with the rest of the engineering team to improve APIs, signup flows, and much more.",
+         "Monitor success of mobile app users and help implement changes based on user feedback."
+       ],
+       "requirements": [
+         "A CS degree and/or several years of experience as a software engineer.",
+         "An iOS app (or several) in the App Store that we can take a look at.",
+         "2+ years of experience doing iOS app development.",
+         "Experience with REST APIs - bonus points for architecting or designing the backend for a mobile app.",
+         "A passion for great products and user experiences.",
+         "An unquenchable thirst to help people be successful and express themselves."
+       ],
+       "apply": "Send your GitHub account, links to your work/apps, a note about your experience, and 3 reasons you want to work at Storenvy to <a href='mailto:jobs+engineering@storenvy.com'>jobs+engineering@storenvy.com</a>."
+     },
+
+
     /*  Frontend Engineer  */
     {
       "title": "Front-end Engineer",
@@ -75,5 +101,25 @@ _StorenvyJobs({
       ],
       "apply": "Send your GitHub account, links to your work and a note about your experience to <a href='mailto:jobs+engineering@storenvy.com'>jobs+engineering@storenvy.com</a>."
     }
+  ],
+
+  "Design": [
+  {
+    "title" : "Lead Mobile Designer",
+    "permalink" : 'iosdesign',
+    'pitch' : 'We’re hiring a designer to own the visual experience of our iOS app (now in development). You’ll primarily work on the iOS app, mobile web UI, and eventually Android. This app will be consumer-facing and will be installed by millions of shoppers already using our website.<br/><br/>Storenvy currently doesn’t have an exceptional mobile presence, but we are very committed to a mobile-first approach moving forward. The CEO is very passionate about our mobile products and believes that our native mobile apps will become the core focus of the company in 6-12 months.',
+    'responsibilities' : [
+      'Work with the CEO, engineers, community, and merchandising to build and refine Storenvy\'s native mobile experiences.',
+      'Inform and collaborate on the design of all of Storenvy\'s products and marketing.',
+      'Plan and sequence feature enhancements and product roadmap.'
+    ],
+    'requirements' : [
+      "You’re a great designer with strong experience designing for interactive mobile experiences.",
+      "You straddle a mixture of UX, UI, and brand design. Our mobile apps should feel very branded and unique from the the vanilla iOS experience.",
+      "You exude empathy for the user, their real-world problems, and how they’ll interact with the product.",
+      "You research and obsess over amazing mobile products and have developed a high bar for design quality in the work you ship."
+    ],
+    'apply' : 'Send a note, your LinkedIn, design portfolio, and links to apps you\'ve design to <a href="mailto:jobs+design@storenvy.com">jobs+design@storenvy.com</a>'
+  }
   ]
 });
