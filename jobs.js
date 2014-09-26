34704bf0fe67cb38816607fd07f2e7ee47bee9bc
Jon Crawford <mail@joncrawford.com>
2014-09-25 18:13:50 -0700

    Hired VP of Engineering!

--- jobs.js
+++ jobs.js
@@ -5,29 +5,6 @@ _StorenvyJobs({
                                       Engineering
     \*~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~*/
   "Engineering": [
-
-  {
-    "title": "VP of Engineering",
-    "permalink" : "vpe",
-    "pitch" : "Storenvy is hiring its first VP of Engineering to assume all responsibilities over our engineering team including personnel management, culture development, and workflow design. Being a small team, the ideal candidate would also have strong experience with Ruby on Rails and be excited to write code regularly as an individual contributor until the engineering team scales to a size that does not allow it. This is a pivotal moment in the development of Storenvy’s product and engineering culture, and this role will allow the ideal candidate to create big impact in a fast-growing company serving an important mission.<br/><br/>Our Engineering Culture<br/>First and foremost, this is your show. When you hop into your role at Storenvy, you will be allowed to define the engineering culture and process that works best for you to deliver results to the business. Currently, we adhere to strict-ish code review and testing practices with a weekly sprint cycle. We use automated testing and automated code quality review. And all of these details are subject to change under your purview as the VP of Engineering.",
-    "responsibilities" : [
-      "Collaborate with the CEO and other leaders to plan, execute and measure the success of all of Storenvy’s product and engineering objectives across web and mobile.",
-      "Develop a productive and positive engineering culture.",
-      "Plan and execute development cycles.",
-      "Interview and hire engineering candidates.",
-      "Design, build and manage a growing engineering organization.",
-      "Guide key architecture and infrastructure decisions."
-    ],
-    "requirements" : [
-      "3+ years of engineering management at a high-growth technology company.",
-      "A proven track record managing and scaling a productive engineering team.",
-      "A battle-tested recruiting and interview process.",
-      "A passion for delivering great user experiences.",
-      "Experience with the key languages, tools and services used to power a growing technology company."
-    ],
-    "apply" : "For this kind of role, it's so much about culture fit and clicking with the leadership and team. Just drop me (the CEO) an email at <a href=\"mailto:jon@storenvy.com\">jon@storenvy.com</a> and we'll jump on a call or grab a beer and take it from there!"
-  },
-
   {
   /* iOS Engineer */
        "title": "Lead iOS Engineer",
@@ -51,29 +28,6 @@ _StorenvyJobs({
        ],
        "apply": "Send your GitHub account, links to your work/apps, a note about your experience, and 3 reasons you want to work at Storenvy to <a href='mailto:jobs+engineering@storenvy.com'>jobs+engineering@storenvy.com</a>."
      },
-
-
-    /*  Frontend Engineer  */
-    {
-      "title": "Front-end Engineer",
-      "permalink": "frontend",
-      "pitch": "Storenvy is an early-stage product-driven company where design and engineering are core to what we do. We are looking for a front-end engineer that loves building great user interfaces with clean, semantic, modular, structured code and cares deeply about performance, accessibility and progressive enhancement.  You should have a solid background with JavaScript and CSS and understand how tools like jQuery, Backbone and SCSS complement them. You might just have very strong opinions on when to use prototypical versus pseudo-classical patterns, or Base64 encoding versus sprites. Most importantly, you should care about the user and how they will interact with your code.",
-      "responsibilities": ["Work with our small team of front-end and back-end engineers to build new user interfaces using jQuery, Backbone, SCSS, etc.",
-        "Take designs from PSD/Fireworks to HTML, CSS and JavaScript.",
-        "Work with back-end engineers to implement application logic.",
-        "Write performant, efficient front-end code.",
-        "Monitor front-end performance and make improvements.",
-        "Implement analytics tools to understand how users interact with the application."
-      ],
-      "requirements": ["3+ years experience writing great front-end code.",
-        "Strong competency in JavaScript and CSS.",
-        "Experience with Git/Github.",
-        "Great communication and teamwork skills.",
-        "Experience in Ruby on Rails a plus.",
-        "Design skills a plus."
-      ],
-      "apply": "Send your GitHub account, links to your work and a note about your experience to <a href='mailto:jobs+frontend@storenvy.com'>jobs+frontend@storenvy.com</a>."
-    },
     /* Senior Rails Engineer  */
     {
       "title": "Full-stack Engineer (Rails)",
